9df1cbcf0f5f8fc23e64f6d13ba56df647925e1a
edwardjacksonmy <88822099+edwardjacksonmy@users.noreply.github.com>
2023-08-24 22:14:06 +0800

    Delete tblUserAccounts.sql

--- tblUserAccounts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `java2`.`useraccount` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(45) NOT NULL,
-    `password` VARCHAR(45) NOT NULL,
-    UNIQUE INDEX `id_UNIQUE` (`id`) ,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `username_UNIQUE` (`username`) );
